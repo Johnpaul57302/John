3b208cfc7b0982997bcf7eb9233eb2075fc83237
Johnpaul57302 <88427432+Johnpaul57302@users.noreply.github.com>
2021-08-17 12:35:32 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,16 @@
+package com.Kluniversity;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        //Variables
+        String name="Mounish";
+        int age=99;
+        String neighbour="Appu";
+        String friend=name;
+        System.out.println(name);
+        System.out.println(neighbour);
+        System.out.println(age);
+        System.out.println(friend+neighbour);
+    }
+}
---
+++ do_while.java
@@ -0,0 +1,12 @@
+package com.java.java;
+public class do_while {
+    public static void main(String[] args) {
+        int i=1;
+                do {
+                    System.out.println("i:" + i);
+                    i = i + 1;
+                }
+                while (i<5);
+    }
+}
+
